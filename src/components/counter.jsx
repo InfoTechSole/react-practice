4c52040338d8eb59faa839d151e3ed7c555a748f
InfoTechSole <bitf11m033@gmail.com>
2021-06-20 12:24:43 +0500

    3-Components 6th lecture -- Refactor code which was added accidently/automatically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -14,18 +14,16 @@ class Counter extends Component {
   render() {
     return (
       <React.Fragment>
-        {/* React.Fragment is child of 'React' */}
+        {/* React.Fragment is child of 'React' module */}
         <img src={this.state.imageUrl} alt="Picsum" />
         {/* <img src="https://picsum.photos/200" /> */}
         {/* If we use double quotes for 'src' then its value will render as 'plain text' */}
         <span style={this.styles} className="badge badge-primary m-2">
-          {" "}
-          {this.formatCount()}{" "}
+          {this.formatCount()}
         </span>
         {/* 'class' is reserved word in js/jsx so we need to use 'className' for html class attribute */}
         <button style={{ fontSize: 16 }} className="btn btn-secondary btn-sm">
-          {" "}
-          Increment{" "}
+          Increment
         </button>
       </React.Fragment>
     );
