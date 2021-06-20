6cd3847ff60f142a29868f9f502a44baad579a1d
InfoTechSole <bitf11m033@gmail.com>
2021-06-20 14:54:44 +0500

    3-Components 14th lecture

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,20 +6,25 @@ class Counter extends Component {
   };
 
   // To updte 'state' we need to use one of method 'setState' inherited from base 'Component' of react
-  handleIncrement = () => {
-    // console.log("Increment Clicked", this);
+  // this.setState() is telling react that the value of this component is going to change.
+  // React then schedule a call to the 'render' method that sometime in the future,
+  // this method is going to be called, we don't know when. This is async call
+
+  handleIncrement = product => {
     this.setState({ count: this.state.count + 1 });
-    // this.setState() is telling react that the value of this component is going to change.
-    // React then schedule a call to the 'render' method that sometime in the future,
-    // this method is going to be called, we don't know when
+    console.log(product);
   };
+
+  //   doHandleIncrement = () => {
+  //     this.handleIncrement({ id: 1 });
+  //   };
 
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
