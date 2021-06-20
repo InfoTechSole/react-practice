0d6b31a3c4b5a3901467bd1e4efdbf785876f71f
InfoTechSole <bitf11m033@gmail.com>
2021-06-20 14:23:16 +0500

    3-Components 11th lecture

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,12 +4,27 @@ class Counter extends Component {
   state = {
     count: 0
   };
+  // One way to access current 'counter' object is to bind event handler to 'this'
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
 
-  handleIncrement() {
-    console.log("Increment Clicked");
-    // console.log(this.state.count); // This line will cause error: can not read property of undefined
-    // 'this' keyword is not accessible in regular javascript function
-  }
+  // 'this' keyword is not accessible in regular javascript function
+  // 'this' in javascript behaves differently as compared to other languages
+  // obj.method() will return reference to current object
+  // function() will return 'window' object,
+  // but if strict mode is enabled then it will return 'undefined'
+  // handleIncrement() {
+  //   console.log("Increment Clicked");
+  //   // console.log(this.state.count); // This line will cause error: can not read property of undefined
+  // }
+
+  //   2nd way is to use arrow function
+  //   Arrow functions don't rebind 'this' keyword, they inherit interopDefault
+  handleIncrement = () => {
+    console.log("Increment Clicked", this);
+  };
 
   render() {
     return (
