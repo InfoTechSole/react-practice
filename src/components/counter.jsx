0b539751349cc7d6a3b70cf628d23df03ba8ee65
InfoTechSole <bitf11m033@gmail.com>
2021-06-20 13:39:37 +0500

    3-Components 8th lecture

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    test: "abc"
+    tags: ["tag1", "tag2", "tag3"]
   };
 
   render() {
@@ -13,6 +13,12 @@ class Counter extends Component {
       <React.Fragment>
         <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
         <button className="btn btn-secondary btn-sm"> Increment </button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+            // key should be unique only for specified/given list/block
+          ))}
+        </ul>
       </React.Fragment>
     );
   }
