d88831abeabee926d26cf54507cf531b3c499dd1
InfoTechSole <bitf11m033@gmail.com>
2021-06-21 08:39:48 +0500

    4-NestedComponents lecture 7  --  Raising and handling events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,23 +1,15 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  // The component that OWNS a piece of the state, should be the one modifying it.
   state = {
     count: this.props.value
   };
-
-  // To updte 'state' we need to use one of method 'setState' inherited from base 'Component' of react
-  // this.setState() is telling react that the value of this component is going to change.
-  // React then schedule a call to the 'render' method that sometime in the future,
-  // this method is going to be called, we don't know when. This is async call
 
   handleIncrement = product => {
     this.setState({ count: this.state.count + 1 });
     console.log(product);
   };
-
-  //   doHandleIncrement = () => {
-  //     this.handleIncrement({ id: 1 });
-  //   };
 
   render() {
     console.log(this.props);
@@ -33,13 +25,25 @@ class Counter extends Component {
     return (
       <React.Fragment>
         <div>
-          {this.props.children} {/* It will print 'Title' */}
+          {this.props.children}
+          {/* Now 'props.children' will print following: */}
+          {/* {id: 1, value: 4, onDelete: ƒ} */}
+          {/* {id: 2, value: 0, onDelete: ƒ} */}
+          {/* {id: 3, value: 0, onDelete: ƒ} */}
+          {/* {id: 4, value: 0, onDelete: ƒ} */}
+
           <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
           <button
             onClick={() => this.handleIncrement()}
             className="btn btn-secondary btn-sm"
           >
             Increment
+          </button>
+          <button
+            className="btn btn-danger btn-sm m-2"
+            onClick={this.props.onDelete}
+          >
+            Delete
           </button>
         </div>
       </React.Fragment>
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -6,6 +6,7 @@ class Counters extends Component {
   // Props includes data that we give to Components. PROPS ARE READONLY
   // Whereas state includes data that is local or private to that component
   // So other components can not access data directly from state
+
   state = {
     counters: [
       { id: 1, value: 4 },
@@ -14,15 +15,22 @@ class Counters extends Component {
       { id: 4, value: 0 }
     ]
   };
+
+  handleDelete = () => {
+    console.log("Delete Clicked");
+  };
+
   render() {
     return (
       <div>
         {this.state.counters.map(counter => (
           // key, value and id are part of the props
-          <Counter key={counter.id} value={counter.value} id={counter.id}>
-            {/* <h4> Title </h4> */}
-            <h4> Counter # {counter.id} </h4>
-          </Counter>
+          <Counter
+            onDelete={this.handleDelete}
+            key={counter.id}
+            id={counter.id}
+            value={counter.value}
+          />
         ))}
       </div>
     );
