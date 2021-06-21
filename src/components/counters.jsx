e30ad3a2ee7cda9919eb331f078dbabeb0636c46
InfoTechSole <bitf11m033@gmail.com>
2021-06-21 08:25:36 +0500

    4-NestedComponents lecture 5, 6  -- Debugging React Apps, and props vs state

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
+  // Difference between props and state?
+  // Props includes data that we give to Components. PROPS ARE READONLY
+  // Whereas state includes data that is local or private to that component
+  // So other components can not access data directly from state
   state = {
     counters: [
       { id: 1, value: 4 },
@@ -14,7 +18,8 @@ class Counters extends Component {
     return (
       <div>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value}>
+          // key, value and id are part of the props
+          <Counter key={counter.id} value={counter.value} id={counter.id}>
             {/* <h4> Title </h4> */}
             <h4> Counter # {counter.id} </h4>
           </Counter>
