cd824235a88d884f21e4e1567bc74829cdba18bf
InfoTechSole <bitf11m033@gmail.com>
2021-06-21 13:37:23 +0500

    4-NestedComponents lecture 11  --  Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
 import "./App.css";
-import Movies from "./components/movies";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Counters />
+      </main>
+    </React.Fragment>
   );
 }
 
